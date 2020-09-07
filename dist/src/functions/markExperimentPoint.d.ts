089024d9a1f477a58134aa0761cae73255bedb16
Jaydip Hirapara <jaydip.hirapara@savvas.com>
2020-09-07 23:44:52 +0530

    Add dist in test package

---
+++ dist/src/functions/markExperimentPoint.d.ts
@@ -0,0 +1,2 @@
+import { Interfaces } from '../identifiers';
+export default function markExperimentPoint(url: string, userId: string, token: string, experimentPoint: string, partitionId?: string): Promise<Interfaces.IMarkExperimentPoint>;
